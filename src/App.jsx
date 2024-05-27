072e68d34104deed4b03803a74d8f88e807e23e3
yash <ymandlekar9826@gmail.com>
2024-05-27 17:11:11 +0530

    by yash

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import Front from './Components/Front'
-import Aboutevent from './Components/Aboutevent'
-import Judge from './Components/Judge'
-import './App.css'
-import Navbar from './Components/Navbar'
-import Sponser from './Components/Sponser'
+import { useState } from "react";
+import Front from "./Components/Front";
+import Aboutevent from "./Components/Aboutevent";
+import Judge from "./Components/Judge";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Sponser from "./Components/Sponser";
 /*import './Components/Navbar.sass'*/
-import Prize from './Components/Prize'
-
+import Prize from "./Components/Prize";
+import LocomotiveScroll from 'locomotive-scroll';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+const locomotiveScroll = new LocomotiveScroll();
+
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -20,9 +23,8 @@ function App() {
       <Judge />
       <Sponser />
       <Prize />
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Front.jsx
+++ src/Components/Front.jsx
@@ -7,7 +7,7 @@ const Front = () => {
       
          <p id="content">
 
-            <p> <span id='aarambhname'>"AARAMBH"</span><br/>
+            <p data-scroll data-scroll-speed="0.5" data-scroll-orientation="horizontal">  <span id='aarambhname' >"AARAMBH"</span><br/>
             Tech-Event<br/>
             3<sup>rd</sup>-5<sup>th</sup>June 2024  <br/>
             <p id="line1">A event for showcase your tallent by code...!</p>
